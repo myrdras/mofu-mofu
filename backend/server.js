06057f173742e3fd231e18de223639d0be992789
Macarena Espindola <61092057+myrdrass@users.noreply.github.com>
2023-12-13 09:18:43 -0300

    Configuro puerto

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,6 @@ app.use((err, req, res, next) => {
   const status = err.name && err.name === 'ValidationError' ? 400 : 500;
   res.status(status).send({ message: err.message });
 });
-app.listen(5000, () => {
-  console.log("server at http://localhost:5000");
+app.listen(config.PORT, () => {
+  console.log("server at http://localhost:"+config.PORT);
 });
